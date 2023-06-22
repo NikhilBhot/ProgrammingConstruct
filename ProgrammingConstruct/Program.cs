1d012d0fb0d09d2bc8778b4e6045952b158bbea0
unknown <bhotnikhil25@gmail.com>
2023-06-22 23:17:43 +0530

    [Nikhil]-Check Vote Eligibility

--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -1,4 +1,6 @@
-﻿namespace ProgrammingConstruct
+﻿using System.Runtime.ConstrainedExecution;
+
+namespace ProgrammingConstruct
 {
     public class Program
     {
@@ -6,7 +8,8 @@
         {
             Console.WriteLine("Wel-Come To Programming Construct");
             Console.WriteLine("1:Check Integer Equal");
-            Console.WriteLine("1:Check Even Or Odd");
+            Console.WriteLine("2:Check Even Or Odd");
+            Console.WriteLine("3:Check Vote Eligibility");
             
             Console.WriteLine("_____________________________");
             Console.WriteLine("Enter Your Choice");
@@ -22,6 +25,11 @@
                case 2:
                     //Write a C# Sharp program to check whether a given number is even or odd..
                     EvenOrOdd.checkEvenOrOdd();
+                    break;
+                case 3:
+                    /*Write a C# Sharp program to read the age of a candidate and
+                    determine whether it is eligible for casting his/ her own vote.*/
+                    Voteeligiblity.voteEligibility();
                     break;
                 default:
                     Console.WriteLine("Please Enter Correct Choice");
