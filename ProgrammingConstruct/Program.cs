cf105cef2e654d7757b42d097ecdd67dec23f991
unknown <bhotnikhil25@gmail.com>
2023-06-22 22:40:51 +0530

    [Nikhil]-Check Integer Equal

--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -5,6 +5,25 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Wel-Come To Programming Construct");
+            Console.WriteLine("1:Check Integer Equal");
+            Console.WriteLine();
+            Console.WriteLine("_____________________________");
+            Console.WriteLine("Enter Your Choice");
+            int choice = int.Parse(Console.ReadLine());
+
+            switch (choice)
+            {
+                case 1:
+                    //1.Write a C# Sharp program to accept two integers and check whether they are equal or not.
+
+                    IntegerEquality.checkIntEqual();
+                    break;
+                default:
+                    Console.WriteLine("Please Enter Correct Choice");
+                    break;
+
+            }
+           
         }
     }
 }
