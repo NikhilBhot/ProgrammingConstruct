2a64865729f1ee8923132cd642753c64160287ac
unknown <bhotnikhil25@gmail.com>
2023-06-22 23:09:53 +0530

    [Nikhil]-Check The Number Is Even or Odd

--- ProgrammingConstruct/Program.cs
+++ ProgrammingConstruct/Program.cs
@@ -6,7 +6,8 @@
         {
             Console.WriteLine("Wel-Come To Programming Construct");
             Console.WriteLine("1:Check Integer Equal");
-            Console.WriteLine();
+            Console.WriteLine("1:Check Even Or Odd");
+            
             Console.WriteLine("_____________________________");
             Console.WriteLine("Enter Your Choice");
             int choice = int.Parse(Console.ReadLine());
@@ -17,6 +18,10 @@
                     //1.Write a C# Sharp program to accept two integers and check whether they are equal or not.
 
                     IntegerEquality.checkIntEqual();
+                    break;
+               case 2:
+                    //Write a C# Sharp program to check whether a given number is even or odd..
+                    EvenOrOdd.checkEvenOrOdd();
                     break;
                 default:
                     Console.WriteLine("Please Enter Correct Choice");
